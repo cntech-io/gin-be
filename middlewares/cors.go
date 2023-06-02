29a6fe48aab7a28c69fa02c317467a5ed4bf6ab2
yunus <yunus@cntech.io>
2023-06-02 15:02:29 +0300

    some info added for cors middleware

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -1,14 +1,18 @@
 package middlewares
 
 import (
+	"fmt"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func CORSMiddleware(config *cors.Config) gin.HandlerFunc {
 	if config == nil {
+		fmt.Println("default cors config applied")
 		return cors.Default()
 	} else {
+		fmt.Println("new cors config applied")
 		return cors.New(*config)
 	}
 }
